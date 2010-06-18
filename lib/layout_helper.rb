24562db37ba0eccd3faf46d79ccdf567f00fc2e5
Andi Altendorfer <andi@iboard.cc>
2010-06-18 15:44:31 +0200

    Add String Extensions

--- lib/layout_helper.rb
+++ lib/layout_helper.rb
@@ -25,6 +25,21 @@ module LayoutHelper
       rc += "</div>".html_safe
       rc
     end
-  
+    
 end
+
+module StringExtensions
+  RAND_CHARS = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789"
+  def random_string(len)
+      rand_max = RAND_CHARS.size
+      ret = "" 
+      len.times{ ret << RAND_CHARS[rand(rand_max)] }
+      ret 
+  end
+end
+
+class String
+  extend StringExtensions
+end
+
 
